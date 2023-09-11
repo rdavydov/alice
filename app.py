c58bcdcd63b7133687caa614b5b71fe52b9e89e6
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2023-09-11 13:55:56 +0300

    pwd check

--- app.py
+++ app.py
@@ -10,76 +10,91 @@ import io
 # Установка кодировки UTF-8 для консоли
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-
 app = Flask(__name__)
 
 # Настроим логирование
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Проверим, существует ли таблица "users", и создадим её, если нет
+conn = sqlite3.connect('passwords.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        user_id TEXT PRIMARY KEY,
+        password TEXT,
+        name TEXT
+    )
+''')
+conn.commit()
 
-def create_user(user_id, password):
+# Функции
+# ---------------------------------
+def create_user(user_id, password, name):
     conn = sqlite3.connect('passwords.db')
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (user_id, password) VALUES (?, ?)", (user_id, password))
+    cursor.execute("INSERT INTO users (user_id, password, name) VALUES (?, ?, ?)", (user_id, password, name))
     conn.commit()
     conn.close()
 
-def check_password(user_id, password):
+def check_password(password):
     conn = sqlite3.connect('passwords.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT password FROM users WHERE user_id=?", (user_id,))
+    cursor.execute("SELECT name FROM users WHERE password=?", (password))
     result = cursor.fetchone()
     conn.close()
     if result is None:
         return False
-    return result[0] == password
-
-import json
-
-# ...
+    return result[0]
 
 def pretty_print(data):
     return json.dumps(data, ensure_ascii=False, indent=4)
+# ---------------------------------
+
+# Тестовые пользователи
+########################################
+create_user('1', '12345', 'Роман')
+create_user('2', '6789', 'Иван')
+########################################
 
 @app.route('/alice', methods=['POST'])
 def alice():
     data = request.json
     user_id_request = data['session']['user_id']
-    text = data['request']['original_utterance']
+
+    if data['request'] and data['request']['original_utterance'] and len(data['request']['original_utterance']) > 0:
+        input_text = data['request']['original_utterance'].lower()  # Приведем текст к нижнему регистру для удобства сравнения
+
+        if check_password(input_text):
+            name = check_password(input_text)
+            res = {
+                'version': data['version'],
+                'session': data['session'],
+                'response': {
+                    'text': f'Пароль верный, привет, {name}!',
+                    'end_session': False
+                }
+            }
+            return json.dumps(res)
+        else:
+            res = {
+                'version': data['version'],
+                'session': data['session'],
+                'response': {
+                    'text': 'Пароль неверный',
+                    'end_session': False
+                }
+            }
+            return json.dumps(res)
 
     # Выведем запрос в консоль с отступами и новыми строками
     logging.debug("---------- Request ----------")
     logging.debug(f"User ID (from request): {user_id_request}")
     logging.debug(pretty_print(data))
 
-    # Проверим, существует ли таблица "users", и создадим её, если нет
-    conn = sqlite3.connect('passwords.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            user_id TEXT PRIMARY KEY,
-            password TEXT
-        )
-    ''')
-    conn.commit()
-
-    if data['request'] and data['request']['original_utterance'] and len(data['request']['original_utterance']) > 0:
-        text = data['request']['original_utterance']
-    res = {
-        'version': data['version'],
-        'session': data['session'],
-        'response': {
-            'text': text,
-            'end_session': False
-        }
-    }
-
-
     # Выведем ответ в консоль с отступами и новыми строками
     logging.debug("---------- Response ----------")
     logging.debug(pretty_print(res))
 
-    conn.close()  # Закрываем соединение с базой данных
     return json.dumps(res)
 
 
