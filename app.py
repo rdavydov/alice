e66f027209dc1df0994c6b947d329380daa99c1c
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2023-09-17 22:42:31 +0300

    help question handling

--- app.py
+++ app.py
@@ -61,7 +61,58 @@ def pretty_print(data):
 # create_user('2', 'ястреб', 'Иван')
 # create_user('3', 'барс', 'Пётр')
 ########################################
+'''
+@app.route('/alice', methods=['POST'])
+def main():
+    # logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(request.json, response)
+    # logging.info(f'Response:  {response!r}')
+    return json.dumps(response)
 
+def handle_dialog(req, res): # диалог с пользователем
+    user_id = req['session']['user_id']
+    if req['session']['new']: # новая сессия
+        res['response'][
+            'text'] = 'Привет! Я тестовый навык для Алисы! ' + \
+            'Я могу идентифицировать пользователей!'
+        return
+
+    try: # на всякий случай)
+        if list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['закончить', 'диалог'] or \
+           list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['завершить', 'диалог']:
+            res['response']['text'] = 'Пока! Возвращайся снова!'
+            res['response']['tts'] = 'Пока! Возвращайся снова!'
+            res['response']['end_session'] = True
+            return
+    except Exception:
+        pass
+    try:
+        # что ты умеешь? - обязательный вопрос в навыке Алисы
+        if list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['что', 'ты', 'умеешь'] or \
+           list(map(lambda x: x.lower(), req['request']['nlu']['tokens'])) == ['помощь']:
+            res['response']['text'] = 'Я умею идентифицировать пользователей ' + \
+                'по тем кодовым словам (паролям), которые они мне назовут'
+            return
+    except Exception:
+        pass
+    try:
+        name = check_password(req['request']['command'])
+        if name:
+            res['response']['text'] = f'Пароль верный, привет, {name}!'
+        else:
+            res['response']['text'] = 'Пароль неверный'
+        return
+    except Exception as e:
+        # res['response']['text'] = "Не совсем тебя поняла."
+        res['response']['text'] = f"Не совсем тебя поняла. Исключение: {e}"
+'''
 @app.route('/alice', methods=['POST'])
 def alice():
     data = request.json
@@ -75,6 +126,20 @@ def alice():
     if data['request'] and data['request']['command'] and len(data['request']['command']) > 0:
         input_text = data['request']['command']
         logging.info(f"input_text: {input_text}")
+
+        # что ты умеешь? - обязательный вопрос в навыке Алисы
+        if list(map(lambda x: x.lower(), data['request']['nlu']['tokens'])) == ['что', 'ты', 'умеешь'] or \
+           list(map(lambda x: x.lower(), data['request']['nlu']['tokens'])) == ['помощь']:
+            res = {
+                'version': data['version'],
+                'session': data['session'],
+                'response': {
+                    'text': 'Я умею идентифицировать пользователей ' + \
+                'по тем кодовым словам (паролям), которые они мне назовут',
+                    'end_session': False
+                }
+            }
+            return json.dumps(res)
 
         if check_password(input_text):
             name = check_password(input_text)
@@ -121,4 +186,6 @@ def alice():
 
 
 if __name__ == '__main__':
+    # con = sqlite3.connect('data_covid.db', check_same_thread=False)  # база данных
+    # cur = con.cursor()
     app.run(host='0.0.0.0', port=80)
